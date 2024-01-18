5584f8caab99d8e489242bec0215382c7931e303
Aarnow <arnaudfgr.dev@gmail.com>
2024-01-18 14:28:29 +0100

    Initial Project Files

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using Life;
+using UnityEngine;
+
+namespace MyBiz
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api): base(api)
+        {
+
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+            //MyMenu
+
+            Debug.Log($"Plugin \"MyBiz\" initialisé avec succès.");
+        }
+    }
+}
