a8d229d74d58b14af66cf548a0431ad9ae7b8d4b
Aarnow <arnaudfgr.dev@gmail.com>
2024-01-18 17:17:25 +0100

    refacto

--- Main.cs
+++ Main.cs
@@ -3,11 +3,7 @@ using Life.UI;
 using System;
 using UnityEngine;
 using MyMenu.Entities;
-using Life.Network;
-using UIPanelManager;
-using Life.DB;
-using System.Collections.Generic;
-using System.Linq;
+using MyBiz.Panels;
 
 namespace MyBiz
 {
@@ -26,7 +22,7 @@ namespace MyBiz
             try
             {
                 Section playerSection = new Section(Section.GetSourceName(), Section.GetSourceName(), "v1.0.0", "Aarnow");
-                Action<UIPanel> playerAction = ui => Open(playerSection.GetPlayer(ui));
+                Action<UIPanel> playerAction = ui => PlayerPanels.Open(playerSection.GetPlayer(ui));
                 playerSection.OnlyAdmin = false;
                 playerSection.Line = new UITabLine(playerSection.Title, playerAction);
                 playerSection.Insert(false);
@@ -37,31 +33,6 @@ namespace MyBiz
             }
 
             Debug.Log($"Plugin \"MyBiz\" initialisé avec succès.");
-        }
-
-        public void Open(Player player)
-        {
-            UIPanel panel = new UIPanel("MyBiz", UIPanel.PanelType.TabPrice).SetTitle("Répertoire des sociétés");
-
-            foreach(Bizs biz in Nova.biz.bizs)
-            {
-                bool isOpen = false;
-                List<Player> players = Nova.server.GetAllInGamePlayers().Where(p => p.biz.Id == biz.Id).ToList();
-                foreach(Player currPlayer in players)
-                {
-                    if (currPlayer.serviceMetier)
-                    {
-                        isOpen = true;
-                        break;
-                    }
-                }
-
-                panel.AddTabLine($"{biz.BizName}", $"{(!isOpen ? $"<color={PanelManager.Colors[NotificationManager.Type.Error]}>fermé</color>": $"<color={PanelManager.Colors[NotificationManager.Type.Success]}>ouvert</color>")}", -1, ui => Debug.Log("open biz"));
-            }
-
-            panel.AddButton("Fermer", ui => PanelManager.Quit(ui, player));
-
-            player.ShowPanelUI(panel);
         }
     }
 }
---
+++ Panels/PlayerPanels.cs
@@ -0,0 +1,39 @@
+﻿using Life.DB;
+using Life;
+using System.Linq;
+using UIPanelManager;
+using Life.UI;
+using Life.Network;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace MyBiz.Panels
+{
+    abstract class PlayerPanels
+    {
+        public static void Open(Player player)
+        {
+            UIPanel panel = new UIPanel("MyBiz", UIPanel.PanelType.TabPrice).SetTitle("Répertoire des sociétés");
+
+            foreach (Bizs biz in Nova.biz.bizs)
+            {
+                bool isOpen = false;
+                List<Player> players = Nova.server.GetAllInGamePlayers().Where(p => p.biz.Id == biz.Id).ToList();
+                foreach (Player currPlayer in players)
+                {
+                    if (currPlayer.serviceMetier)
+                    {
+                        isOpen = true;
+                        break;
+                    }
+                }
+                panel.AddTabLine($"{biz.BizName}", $"{(!isOpen ? $"<color={PanelManager.Colors[NotificationManager.Type.Error]}>fermé</color>" : $"<color={PanelManager.Colors[NotificationManager.Type.Success]}>ouvert</color>")}", -1, ui => Debug.Log("open biz"));
+            }
+
+            panel.AddButton("Retour", ui => PanelManager.NextPanel(player, ui, () => MyMenu.Panels.PlayerPanels.OpenMenuPanel(player)));
+            panel.AddButton("Fermer", ui => PanelManager.Quit(ui, player));
+
+            player.ShowPanelUI(panel);
+        }
+    }
+}
