55a8dd9f45b89eab08b9699e9087c6e12499052f
Aarnow <arnaudfgr.dev@gmail.com>
2024-01-18 15:42:16 +0100

    BizPanel State

--- Main.cs
+++ Main.cs
@@ -3,6 +3,11 @@ using Life.UI;
 using System;
 using UnityEngine;
 using MyMenu.Entities;
+using Life.Network;
+using UIPanelManager;
+using Life.DB;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyBiz
 {
@@ -21,7 +26,7 @@ namespace MyBiz
             try
             {
                 Section playerSection = new Section(Section.GetSourceName(), Section.GetSourceName(), "v1.0.0", "Aarnow");
-                Action<UIPanel> playerAction = ui => Debug.Log("open mybiz panel");
+                Action<UIPanel> playerAction = ui => Open(playerSection.GetPlayer(ui));
                 playerSection.OnlyAdmin = false;
                 playerSection.Line = new UITabLine(playerSection.Title, playerAction);
                 playerSection.Insert(false);
@@ -32,6 +37,31 @@ namespace MyBiz
             }
 
             Debug.Log($"Plugin \"MyBiz\" initialisé avec succès.");
+        }
+
+        public void Open(Player player)
+        {
+            UIPanel panel = new UIPanel("MyBiz", UIPanel.PanelType.TabPrice).SetTitle("Répertoire des sociétés");
+
+            foreach(Bizs biz in Nova.biz.bizs)
+            {
+                bool isOpen = false;
+                List<Player> players = Nova.server.GetAllInGamePlayers().Where(p => p.biz.Id == biz.Id).ToList();
+                foreach(Player currPlayer in players)
+                {
+                    if (currPlayer.serviceMetier)
+                    {
+                        isOpen = true;
+                        break;
+                    }
+                }
+
+                panel.AddTabLine($"{biz.BizName}", $"{(!isOpen ? $"<color={PanelManager.Colors[NotificationManager.Type.Error]}>fermé</color>": $"<color={PanelManager.Colors[NotificationManager.Type.Success]}>ouvert</color>")}", -1, ui => Debug.Log("open biz"));
+            }
+
+            panel.AddButton("Fermer", ui => PanelManager.Quit(ui, player));
+
+            player.ShowPanelUI(panel);
         }
     }
 }
