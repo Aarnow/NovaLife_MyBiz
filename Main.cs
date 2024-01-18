e599d86a888e5d74de90aae6b7812a564d2c1b0f
Aarnow <arnaudfgr.dev@gmail.com>
2024-01-18 14:48:55 +0100

    Import MyMenu

--- Main.cs
+++ Main.cs
@@ -1,5 +1,8 @@
 ﻿using Life;
+using Life.UI;
+using System;
 using UnityEngine;
+using MyMenu.Entities;
 
 namespace MyBiz
 {
@@ -15,6 +18,18 @@ namespace MyBiz
             base.OnPluginInit();
 
             //MyMenu
+            try
+            {
+                Section playerSection = new Section(Section.GetSourceName(), Section.GetSourceName(), "v1.0.0", "Aarnow");
+                Action<UIPanel> playerAction = ui => Debug.Log("open mybiz panel");
+                playerSection.OnlyAdmin = false;
+                playerSection.Line = new UITabLine(playerSection.Title, playerAction);
+                playerSection.Insert(false);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
 
             Debug.Log($"Plugin \"MyBiz\" initialisé avec succès.");
         }
